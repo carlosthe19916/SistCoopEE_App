20d0594fca5d34eb4372bc7ff5329893c21d4bf8
carlos <carlosthe19916@gmail.com>
2015-02-14 08:47:57 -0500

    ABRIR CAJA

--- app/scripts/organizacion/controllers/sucursal/agencia/caja/CajaAbrirCtrl.js
+++ app/scripts/organizacion/controllers/sucursal/agencia/caja/CajaAbrirCtrl.js
@@ -4,18 +4,38 @@ define(['../../../module'], function (module) {
     module.controller('CajaAbrirCtrl', function($scope, $state, Notifications){
 
         $scope.loadParams = function(){
-            $scope.view.boveda = $scope.params.object;
-            $scope.view.boveda.detalle = $scope.view.boveda.$getDetalle().$object;
+            $scope.view.caja = $scope.params.object;
+            $scope.view.caja.$getDetalle().then(function(response){
+                $scope.view.caja.detalle = response;
+
+                angular.forEach($scope.view.caja.detalle, function(row){
+                    row.total = 0;
+                    angular.forEach(row.detalleHistorial, function(subRow){
+                        subRow.getSubTotal = function(){
+                            return this.valor * this.cantidad;
+                        };
+                        row.total = row.total + (subRow.valor * subRow.cantidad);
+                    });
+                });
+
+                console.log($scope.view.caja.detalle);
+            });
         };
         $scope.loadParams();
 
         $scope.abrir = function(){
+            if($scope.view.cajaDB.abierto == true){
+                Notifications.warn('Caja abierta, no se puede abrir nuevamente.');
+                return;
+            }
             if ($scope.form.$valid) {
                 $scope.blockControl();
-                $scope.view.boveda.$abrir().then(
+                $scope.view.cajaDB.$abrir().then(
                     function(response){
                         $scope.unblockControl();
-                        Notifications.success('Boveda abierta');
+                        Notifications.success('Caja abierta');
+                        $scope.view.cajaDB.abierto = true;
+                        $scope.view.caja = angular.copy($scope.view.cajaDB);
                         $state.go('^.resumen');
                     },
                     function error(error){
@@ -27,4 +47,5 @@ define(['../../../module'], function (module) {
         };
 
     });
+
 });
--- app/scripts/organizacion/controllers/sucursal/agencia/caja/CajaCerrarCtrl.js
+++ app/scripts/organizacion/controllers/sucursal/agencia/caja/CajaCerrarCtrl.js
@@ -4,18 +4,36 @@ define(['../../../module'], function (module) {
     module.controller('CajaCerrarCtrl', function($scope, $state, Notifications){
 
         $scope.loadParams = function(){
-            $scope.view.boveda = $scope.params.object;
-            $scope.view.boveda.detalle = $scope.view.boveda.$getDetalle().$object;
+            $scope.view.caja = $scope.params.object;
+            $scope.view.caja.$getDetalle().then(function(response){
+                $scope.view.caja.detalleOld = response;
+                angular.forEach($scope.view.caja.detalleOld, function(row){
+                    row.total = 0;
+                    angular.forEach(row.detalleHistorial, function(subRow){
+                        subRow.getSubTotal = function(){
+                            return this.valor * this.cantidad;
+                        };
+                        row.total = row.total + (subRow.valor * subRow.cantidad);
+                    });
+                });
+                $scope.view.caja.detalleNew = angular.copy($scope.view.caja.detalleOld);
+            });
         };
         $scope.loadParams();
 
         $scope.abrir = function(){
+            if($scope.view.cajaDB.abierto == true){
+                Notifications.warn('Caja abierta, no se puede abrir nuevamente.');
+                return;
+            }
             if ($scope.form.$valid) {
                 $scope.blockControl();
-                $scope.view.boveda.$cerrar().then(
+                $scope.view.cajaDB.$abrir().then(
                     function(response){
                         $scope.unblockControl();
-                        Notifications.success('Boveda cerrada');
+                        Notifications.success('Caja abierta');
+                        $scope.view.cajaDB.abierto = true;
+                        $scope.view.caja = angular.copy($scope.view.cajaDB);
                         $state.go('^.resumen');
                     },
                     function error(error){
